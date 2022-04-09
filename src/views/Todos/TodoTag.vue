f61764a2f0298260d7119e6720407b5c74e151d0
kinzaahmed-web <kinza1013@yahoo.com>
2022-04-09 12:57:26 -0500

    todo tag page looks okay

--- src/views/Todos/TodoTag.vue
+++ src/views/Todos/TodoTag.vue
@@ -4,9 +4,9 @@
 			<router-link to="/todos">
 				<b-button> Go back to all todos! </b-button>
 			</router-link>
-			<main class="container card shadow shadow-lg--hover mt-5" id="todolist">
+			<main class="container card shadow shadow-lg--hover m-3" id="todolist">
 				<div class="row mb-3">
-					<h3 class="mt-3 ml-3">{{ tag }} : get productive now :)</h3>
+					<h3 class="mt-3 ml-3">{{ tag }} task(s)</h3>
 				</div>
 
 				<div class="row">
@@ -42,20 +42,28 @@
 					</div>
 
 					<div class="col-md-12 mt-3">
-						<b-input-group size="sm" class="mb-2">
-							<b-input-group-prepend is-text>
-								<b-icon icon="search"></b-icon>
-							</b-input-group-prepend>
+						<b-form @submit.stop.prevent>
+							<h3>add a new task in {{ tag }}</h3>
 							<b-form-input
-								type="search"
-								class="new-todo"
-								autofocus
-								autocomplete="off"
-								placeholder="What needs to be done?"
 								v-model="newTodo"
-								@keyup.enter="addTodo"
+								:state="taskValidation"
+								class="mb-2 mr-sm-2 mb-sm-0 mt-2"
+								placeholder="do the laundry.."
+								required
 							></b-form-input>
-						</b-input-group>
+							<b-form-valid-feedback :state="taskValidation">
+								looks good!
+							</b-form-valid-feedback>
+							<b-button
+								@click="addTodo"
+								type="submit"
+								variant="outline-primary"
+								pill
+								class="m-4"
+							>
+								add task
+							</b-button>
+						</b-form>
 					</div>
 				</div>
 				<b-list-group flush class="todo-list mb-4">
@@ -85,13 +93,20 @@
 								<span v-else>{{ todo.text }}</span>
 							</router-link>
 						</div>
-						<b-button pill variant="outline-danger">{{ todo.tag }}</b-button>
+						<b-form-tag
+							v-if="todo.tag"
+							:title="tag"
+							:disabled="true"
+							variant="info"
+							>{{ todo.tag }}</b-form-tag
+						>
 						<b-button
 							variant="outline-primary"
 							title="Delete"
+							pill
 							@click="deleteTask(todo.id)"
 						>
-							<b-icon icon="trash"></b-icon> Delete
+							<b-icon icon="trash"></b-icon> delete
 						</b-button>
 					</b-list-group-item>
 				</b-list-group>
@@ -136,34 +151,28 @@ export default {
 					this.$router.replace("login");
 				});
 		},
-		tagBasedTodos() {
-			return this.tagTodos;
+		deleteTask(id) {
+			this.tagTodos.splice(id, 1);
 		},
-		filteredTodos(filter) {
-			if (filter === "done") {
-				return this.completedTodos;
-			} else if (filter === "pending") {
-				return this.tasksLeft;
-			} else {
-				return this.tagTodos;
+		addTodo: function () {
+			var value = this.newTodo && this.newTodo.trim();
+			if (!value) {
+				return;
 			}
+			var tagValue = this.tag && this.tag.trim();
+			if (!tagValue) {
+				return;
+			}
+			this.todos.push({
+				id: this.tagTodos.length,
+				text: value,
+				complete: false,
+				tag: tagValue,
+			});
+			this.newTodo = "";
 		},
 	},
 	computed: {
-		// deleteTask(id) {
-		// 	this.todos.splice(id, 1);
-		// },
-		// addTodo() {
-		// 	var value = this.newTodo && this.newTodo.trim();
-		// 	if (!value) {
-		// 		return;
-		// 	}
-		// 	this.todos.push({ id: this.todos.length, text: value, complete: false });
-		// 	this.newTodo = "";
-		// // },
-		// tagTodos() {
-		// 	return this.todos.filter((t) => t.tag === this.tag);
-		// },
 		tasksLeft() {
 			return this.tagTodos.filter((t) => !t.complete);
 		},
@@ -181,6 +190,9 @@ export default {
 			} else {
 				return this.tagTodos;
 			}
+		},
+		taskValidation() {
+			return this.newTodo.length > 3 && this.newTodo.length < 21;
 		},
 	},
 };
